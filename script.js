a723d52bc5f592e9a0512c7afae7d8105e800abe
PlatinaStar <a.polasek01@gmail.com>
2024-01-29 13:13:14 +0100

    .

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 x = 1
-for (i = 0 ; i < 18 ; i++) {
+y = false
+for (i = 0 ; i < document.getElementById("gridder").children.length ; i++) {
     document.getElementById("gridder").children[i].style = "grid-row: " + x + ";"
     x++
-    if (x == 10) {
+    if (x == document.querySelectorAll('#gridder .gridderitem').length + 2 && y != true) {
         x = 1
+        y = true
     }
 }
