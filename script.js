2b0724448f49f641b498745b104cb1e79bdb955a
PlatinaStar <a.polasek01@gmail.com>
2024-02-09 11:00:26 +0100

    .

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
 x = 1
 y = false
-for (i = 0 ; i < document.getElementById("gridder").children.length ; i++) {
-    document.getElementById("gridder").children[i].style = "grid-row: " + x + ";"
-    x++
-    if (x == document.querySelectorAll('#gridder .gridderitem').length + 2 && y != true) {
-        x = 1
-        y = true
+if (screen.orientation.type == 'landscape-primary' || screen.orientation.type == 'landscape-secondary'){
+    for (i = 0 ; i < document.getElementById("gridder").children.length ; i++) {
+        document.getElementById("gridder").children[i].classList += "ROWWWS"
+        x++
+        if (x == document.querySelectorAll('#gridder .gridderitem').length + 2 && y != true) {
+            x = 1
+            y = true
+        }
+    }
+}
+else {
+    for (i = 0 ; i < document.getElementById("gridder").children.length ; i++) {
+        document.getElementById("gridder").children[i].style = ""
+        y = false
+    }
+    x = 1
+}
+screen.orientation.onchange = function (){
+    if (screen.orientation.type == 'landscape-primary' || screen.orientation.type == 'landscape-secondary'){
+        for (i = 0 ; i < document.getElementById("gridder").children.length ; i++) {
+            document.getElementById("gridder").children[i].style = "grid-row: " + x + ";"
+            x++
+            if (x == document.querySelectorAll('#gridder .gridderitem').length + 2 && y != true) {
+                x = 1
+                y = true
+            }
+        }
+    }
+    else {
+        for (i = 0 ; i < document.getElementById("gridder").children.length ; i++) {
+            document.getElementById("gridder").children[i].style = ""
+            y = false
+        }
+        x = 0
     }
 }
