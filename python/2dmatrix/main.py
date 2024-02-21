009b022465d435145780d0f50ece45e7f4825749
May_Be_Alex <a.polasek01@gmail.com>
2024-02-21 08:33:51 +0100

    fixed spacing

--- python/2dmatrix/main.py
+++ python/2dmatrix/main.py
@@ -4,42 +4,42 @@ def bubble_sort(unsortedArray):
   n = len(unsortedArray)
   for k in range(n):
     for i in range(len(unsortedArray[k]) - 1):
-        for j in range(len(unsortedArray[k]) - i - 1):
-            if unsortedArray[k][j] > unsortedArray[k][j + 1]:
-                unsortedArray[k][j], unsortedArray[k][j + 1] = unsortedArray[k][j + 1], unsortedArray[k][j]
+      for j in range(len(unsortedArray[k]) - i - 1):
+        if unsortedArray[k][j] > unsortedArray[k][j + 1]:
+          unsortedArray[k][j], unsortedArray[k][j + 1] = unsortedArray[k][j + 1], unsortedArray[k][j]
   return unsortedArray
 
 def selection_sort(unsortedArray):
   n = len(unsortedArray)
   for k in range(n):
     for i in range(len(unsortedArray[k])):
-        min = i
-        for j in range(i+1, n):
-            if unsortedArray[k][j] < unsortedArray[k][min]:
-                min = j
-            unsortedArray[k][i], unsortedArray[k][min] = unsortedArray[k][min], unsortedArray[k][i]
+      min = i
+      for j in range(i+1, n):
+        if unsortedArray[k][j] < unsortedArray[k][min]:
+          min = j
+        unsortedArray[k][i], unsortedArray[k][min] = unsortedArray[k][min], unsortedArray[k][i]
   return unsortedArray
 
 def insertion_sort(unsortedArray):
   for k in range(1, len(unsortedArray)):
     for i in range(1, len(unsortedArray[k])):
-        key = unsortedArray[k][i]
-        j = i - 1
-        while j >= 0 and key < unsortedArray[k][j]:
-            unsortedArray[k][j + 1] = unsortedArray[k][j]
-            j -= 1
-        unsortedArray[k][j + 1] = key
+      key = unsortedArray[k][i]
+      j = i - 1
+      while j >= 0 and key < unsortedArray[k][j]:
+        unsortedArray[k][j + 1] = unsortedArray[k][j]
+        j -= 1
+      unsortedArray[k][j + 1] = key
     return unsortedArray
   
 def partition(unsortedArray, low, high):
-    pivot = unsortedArray[high]
-    i = low - 1
-    for j in range(low, high):
-        if unsortedArray[j] <= pivot:
-            i = i + 1
-            (unsortedArray[i], unsortedArray[j]) = (unsortedArray[j], unsortedArray[i])
-    (unsortedArray[i + 1], unsortedArray[high]) = (unsortedArray[high], unsortedArray[i + 1])
-    return i + 1
+  pivot = unsortedArray[high]
+  i = low - 1
+  for j in range(low, high):
+    if unsortedArray[j] <= pivot:
+      i = i + 1
+      (unsortedArray[i], unsortedArray[j]) = (unsortedArray[j], unsortedArray[i])
+  (unsortedArray[i + 1], unsortedArray[high]) = (unsortedArray[high], unsortedArray[i + 1])
+  return i + 1
  
 def quick_sort(unsortedArray, low, high):
   if low < high:
